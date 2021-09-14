2d87d4b6c6cf9874d56c561ff717c9cc47c2b1d9
Ron Enubs <ronenubs@gmail.com>
2021-09-14 09:39:02 +0800

    this is the original file

---
+++ android/app/src/main/java/com/hellomap101/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hellomap101;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "HelloMap101";
+  }
+}
